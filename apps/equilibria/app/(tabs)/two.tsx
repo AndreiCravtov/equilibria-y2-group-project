4c5329b1275adf1770b1a6fd6f169157ffa95fcf
meshan <mbazarsadaev@yandex.ru>
2025-06-05 23:10:22 +0100

    feat(frontend): started applying our theme

--- apps/equilibria/app/(tabs)/two.tsx
+++ apps/equilibria/app/(tabs)/two.tsx
@@ -1,7 +1,7 @@
 import { Authenticated, Unauthenticated, useQuery, useMutation } from "convex/react";
 import { Text, View, ScrollView, YStack, XStack, Card,
-  Input, FlatList, Button, Icon, Group, H3, useTheme, Separator } from "tamagui";
-import { AlarmClock, ActivitySquare, AirVent } from "@tamagui/lucide-icons";
+  Input, FlatList, Button, Icon, Group, H3, H6, useTheme, Separator } from "tamagui";
+import { AlarmClock, ActivitySquare, AirVent, Edit3 } from "@tamagui/lucide-icons";
 import { api } from "@/convex/_generated/api";
 import { useState } from "react";
 
@@ -30,42 +30,52 @@ export default function TabTwoScreen() {
 
   if (!waterEntries) return <Text>Loading water entries...</Text>;
 
-//   if (waterEntries.length === 0) {
-//     return (
-//       <View flex={1} justifyContent="center" alignItems="center" bg="$background">
-//         <Text>No water records for this user and date in the database!</Text>
-//       </View>
-//     );
-//   }
-
-  console.log(`waterEntries: ${waterEntries}`);
+//   console.log(`waterEntries: ${waterEntries}`);
   const handleAddEntry = async (amount: number | bigint) => {
     if (!user) return;
     await addWater({ uid: user._id, date, water_intake: BigInt(amount) });
     setNewAmount("");
   };
 
+  function createGroupButton({
+    icon,
+    value,
+    bgColor,
+  }: {
+    icon: any
+    value: number
+    bgColor: string
+  }) {
+    console.log(value);
+    return (
+      <Group.Item flex={1}>
+        <Button
+          flex={1}
+          onPress={() => handleAddEntry(value)}
+          icon={icon}
+          color="$blue8Dark"
+          fontWeight="bold"
+          bg={bgColor}
+        >
+          <Text color="$blue8Dark" fontWeight="bold" fontSize="$5">{value}ml</Text>
+        </Button>
+      </Group.Item>
+    )
+  }
+
   return (
-    <ScrollView contentContainerStyle={{ padding: 16 }} bounces={false}>
+    <ScrollView contentContainerStyle={{ padding: 16 }} bounces={false} bg="#FFFFFF">
       <YStack space="$4">
-        <H3 fontWeight="bold" textAlign="center" color={theme.color.indigo10}>Add records</H3>
+        <H3 fontWeight="bold" textAlign="center" color="$indigo8Dark">Add records</H3>
         {/* Input to add water */}
-        <Group orientation="horizontal">
-          <Group.Item>
-            <Button size="$5"onPress={() => handleAddEntry(200)}icon={AlarmClock}>
-              200ml
-            </Button>
-          </Group.Item>
-          <Group.Item>
-            <Button size="$5"onPress={() => handleAddEntry(250)}icon={AlarmClock}>
-              250ml
-            </Button>
-          </Group.Item>
-          <Group.Item>
-            <Button size="$5"onPress={() => handleAddEntry(500)}icon={AlarmClock}>
-              500ml
-            </Button>
-          </Group.Item>
+        <Group orientation="horizontal" width="100%">
+          {[
+            { value: 200, bgColor: '$blue12Dark' },
+            { value: 250, bgColor: '$indigo2' },
+            { value: 500, bgColor: '$blue12Dark' },
+          ].map(({ value, bgColor }) =>
+            createGroupButton({ icon: AlarmClock, value, bgColor })
+          )}
         </Group>
         <YStack space="$2">
           <Input
@@ -73,13 +83,19 @@ export default function TabTwoScreen() {
             keyboardType="numeric"
             value={newAmount}
             onChangeText={setNewAmount}
+            placeholderTextColor="$indigo8Dark"
+            color="$indigo8Dark"
+            bg="$indigo2"
           />
-          <Button onPress={() => handleAddEntry(Number(newAmount))}>Add Entry</Button>
+          <Button onPress={() => handleAddEntry(Number(newAmount))}
+          color="$blue8Dark" bg="$indigo2">
+            Add Entry
+          </Button>
         </YStack>
 
-        <Separator marginVertical={15} />
+        <Separator marginVertical={15} color="indigo8Dark"/>
 
-        <Text fontSize="$6" fontWeight="bold">Water Entries for {date}</Text>
+        <H3 fontWeight="bold" textAlign="center" color="$indigo8Dark">{date} entries</H3>
 
         {/* Show entries */}
         {waterEntries.length === 0 ? (
@@ -87,10 +103,18 @@ export default function TabTwoScreen() {
         ) : (
           waterEntries.map((item) => (
             <Card key={item._id} padded bordered elevate>
-              <Text fontWeight="bold" fontSize="$6">
-                {item.water_intake} mL
-              </Text>
-              <Text>Date: {item.date}</Text>
+              <XStack justifyContent="space-between" alignItems="center">
+                <Text fontWeight="bold" fontSize="$6" color="$indigo8Dark">
+                  {item.water_intake} mL
+                </Text>
+                <Button
+                  size="$3"
+                  chromeless
+                  onPress={() => handleEdit(item)}
+                >
+                <Edit3 size={24} color="$indigo8Dark" />
+                </Button>
+              </XStack>
             </Card>
           ))
         )}
