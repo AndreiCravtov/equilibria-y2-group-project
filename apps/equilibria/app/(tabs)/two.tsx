d412de8035ca1da2ba9de42b336501f816bf598b
meshan <mbazarsadaev@yandex.ru>
2025-06-05 16:03:27 +0100

    feat(add water): added buttons for default water sizes, beautified it a bit

--- apps/equilibria/app/(tabs)/two.tsx
+++ apps/equilibria/app/(tabs)/two.tsx
@@ -1,10 +1,13 @@
 import { Authenticated, Unauthenticated, useQuery, useMutation } from "convex/react";
-import { Text, View, ScrollView, YStack, Card,
-  Input, FlatList, Button } from "tamagui";
+import { Text, View, ScrollView, YStack, XStack, Card,
+  Input, FlatList, Button, Icon, Group, H3, useTheme, Separator } from "tamagui";
+import { AlarmClock, ActivitySquare, AirVent } from "@tamagui/lucide-icons";
 import { api } from "@/convex/_generated/api";
 import { useState } from "react";
 
 export default function TabTwoScreen() {
+  const theme = useTheme();
+//   console.log(theme);
   const user = useQuery(api.users.getCurrentUser);
 
   if (!user) {
@@ -36,18 +39,34 @@ export default function TabTwoScreen() {
 //   }
 
   console.log(`waterEntries: ${waterEntries}`);
-  const handleAddEntry = async () => {
-    const amount = BigInt(newAmount);
-    console.log("inputted amount", amount);
-    await addWater({ uid: user._id, date, water_intake: amount });
+  const handleAddEntry = async (amount: number | bigint) => {
+    if (!user) return;
+    await addWater({ uid: user._id, date, water_intake: BigInt(amount) });
     setNewAmount("");
   };
 
   return (
-    <ScrollView contentContainerStyle={{ padding: 16 }}>
+    <ScrollView contentContainerStyle={{ padding: 16 }} bounces={false}>
       <YStack space="$4">
-
+        <H3 fontWeight="bold" textAlign="center" color={theme.color.indigo10}>Add records</H3>
         {/* Input to add water */}
+        <Group orientation="horizontal">
+          <Group.Item>
+            <Button size="$5"onPress={() => handleAddEntry(200)}icon={AlarmClock}>
+              200ml
+            </Button>
+          </Group.Item>
+          <Group.Item>
+            <Button size="$5"onPress={() => handleAddEntry(250)}icon={AlarmClock}>
+              250ml
+            </Button>
+          </Group.Item>
+          <Group.Item>
+            <Button size="$5"onPress={() => handleAddEntry(500)}icon={AlarmClock}>
+              500ml
+            </Button>
+          </Group.Item>
+        </Group>
         <YStack space="$2">
           <Input
             placeholder="Enter water in mL"
@@ -55,8 +74,10 @@ export default function TabTwoScreen() {
             value={newAmount}
             onChangeText={setNewAmount}
           />
-          <Button onPress={handleAddEntry}>Add Entry</Button>
+          <Button onPress={() => handleAddEntry(Number(newAmount))}>Add Entry</Button>
         </YStack>
+
+        <Separator marginVertical={15} />
 
         <Text fontSize="$6" fontWeight="bold">Water Entries for {date}</Text>
 
--- apps/equilibria/tamagui.config.ts
+++ apps/equilibria/tamagui.config.ts
@@ -1,7 +1,17 @@
-import { defaultConfig } from "@tamagui/config/v4";
 import { createTamagui } from "tamagui";
+import { defaultConfig } from "@tamagui/config/v4";
+import { themes } from "@tamagui/themes";
 
-export const tamaguiConfig = createTamagui(defaultConfig);
+export const tamaguiConfig = createTamagui({
+  ...defaultConfig,
+  theme: {
+    ...defaultConfig.theme,
+    color: {
+      ...defaultConfig.theme.color,
+      ...indigoColors, // merge indigo colors here
+    },
+  },
+});
 
 export default tamaguiConfig;
 
